ecd2910375122f8b3ac4a015d102fc4a335acccb
duongsau <duongsau1211@gmail.com>
2021-04-06 14:52:30 +0700

    a

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
 const path = require('path');
 const mongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://duongsau1211:dolananh@cluster0.zq7aa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://duongsau1211:dolananh@cluster0.zq7aa.mongodb.net/?retryWrites=true&w=majority";
 var routes = require('./src/nodejs/routes/routes');
 app.use(bodyParser.json());
 app.use(cors());
@@ -24,10 +25,9 @@ app.get('/', (req, res) => {
         });
     });
 })
-var portscanner = require('portscanner');
-
-
     app.listen(PORT, () => {
         console.log(`Example app listening at http://localhost:${PORT}`)
     });
+
+
 
