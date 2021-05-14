00da67e190b06db26cb0001e1cb1e53a98ddaa65
Hoang Long <truonghoanglong305@gmail.com>
2021-05-14 14:59:05 +0700

    update react function

--- src/nodejs/storage/images/storage.js
+++ src/nodejs/storage/images/storage.js
@@ -1,7 +1,7 @@
 var multer = require('multer')
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-  cb(null, `src/nodejs/assets/images/upload/${process.loginIndex}`)  
+  cb(null, `src/nodejs/assets/images/upload`)  
   },
   filename: function (req, file, cb) {
     var fileName = file.originalname;
