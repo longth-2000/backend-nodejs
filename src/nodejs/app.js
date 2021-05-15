1a5fbeb3ac066f1f9138d43ba3bda6e6f70241f9
Hoang Long <truonghoanglong305@gmail.com>
2021-05-15 21:26:43 +0700

    create chat

--- src/nodejs/app.js
+++ src/nodejs/app.js
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const multer = require('multer')
 const path = require("path")
-const server = app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.use(cors());
