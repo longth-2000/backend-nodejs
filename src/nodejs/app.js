6c5a05e49d7666fd8135096f3e1ce9b70b2d4ca2
Hoang Long <truonghoanglong305@gmail.com>
2021-05-18 22:03:45 +0700

    add chat

--- src/nodejs/app.js
+++ src/nodejs/app.js
@@ -17,13 +17,17 @@ const database = require("../nodejs/config/database")
 database.connect()
 const io = require('socket.io')(server);
 io.on('connection', function (socket) {
-  socket.on('SEND_MESSAGE_TO_FRIEND', function (data) {
-    socket.broadcast.emit('MESSAGE_TO_FRIEND', data);
-    console.log(data)
-  });
-   socket.on('SEND_MESSAGE_TO_HOST', function (data) {
+  
+   socket.on("join-room", data => {
+    /* socket.join(data) */
+    console.log("Bạn đã tham gia room: " + data)
+  }) 
+  socket.on("SEND_MESSAGE_TO_FRIEND", data => {
+    socket.to("609e3cb4501da742c855dc98").emit("MESSAGE", data)
+  })
+  socket.on('SEND_MESSAGE_TO_HOST', function (data) {
     io.to(socket.id).emit('MESSAGE_TO_HOST', data);
-  }); 
+  });
 });
 
 
