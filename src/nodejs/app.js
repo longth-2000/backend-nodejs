9bf93fb3b21f31acfb5de93f890488b84df10cc0
Hoang Long <truonghoanglong305@gmail.com>
2021-05-18 10:17:09 +0700

    add chat

--- src/nodejs/app.js
+++ src/nodejs/app.js
@@ -16,11 +16,14 @@ routers(app)
 const database = require("../nodejs/config/database")
 database.connect()
 const io = require('socket.io')(server);
-
 io.on('connection', function (socket) {
-  socket.on('SEND_MESSAGE', function (data) {
-    io.emit('MESSAGE', data);
+  socket.on('SEND_MESSAGE_TO_FRIEND', function (data) {
+    socket.broadcast.emit('MESSAGE_TO_FRIEND', data);
+    console.log(data)
   });
+   socket.on('SEND_MESSAGE_TO_HOST', function (data) {
+    io.to(socket.id).emit('MESSAGE_TO_HOST', data);
+  }); 
 });
 
 
