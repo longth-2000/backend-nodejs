64a8c7ee4b49f76369102fce347e1613550313df
Hoang Long <truonghoanglong305@gmail.com>
2021-06-09 11:42:48 +0700

    finalize

--- src/nodejs/auth/profileValidation.js
+++ src/nodejs/auth/profileValidation.js
@@ -4,27 +4,33 @@ const profileValidation = function (data) {
         Fullname: Joi.string()
             .required()
             .messages({
-                'string.empty': `Họ và tên không được để trống`
+                'string.empty': `Họ và tên không được để trống`,
+                'any.required': `Họ và tên không được để trống`
             }),
         Province: Joi.string()
             .required()
             .messages({
-                'string.empty': `Tỉnh không được để trống`
+                'string.empty': `Tỉnh không được để trống`,
+                'any.required': `Họ và tên không được để trống`
             }),
         District: Joi.string()
             .required()
             .messages({
-                'string.empty': `Huyện không được để trống`
+                'string.empty': `Huyện không được để trống`,
+                'any.required': `Huyện không được để trống`
             }),
         Sex: Joi.string()
             .required()
             .messages({
-                'string.empty': `Giới tính không được để trống`
+                'string.empty': `Giới tính không được để trống`,
+                'any.required': `Giới tính không được để trống`
             }),
+           
         Level: Joi.string()
             .required()
             .messages({
-                'string.empty': `Trình độ học vấn được để trống`
+                'string.empty': `Trình độ học vấn được để trống`,
+                'any.required': `Trình độ học vấn được để trống`
             }),
 
     }).options({ abortEarly: false });
