382101384f8d4348460fcd47a25d0c2d15406be1
Hoang Long <truonghoanglong305@gmail.com>
2021-06-09 10:54:10 +0700

    finalize

--- src/nodejs/storage/images/storage.js
+++ src/nodejs/storage/images/storage.js
@@ -1,7 +1,7 @@
 var multer = require('multer')
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-   cb(null, `src/nodejs/assets/images/upload/${req.cookies.token}`) 
+   cb(null, `src/nodejs/assets/images`) 
   },
   filename: function (req, file, cb) {
     var fileName = file.originalname;
