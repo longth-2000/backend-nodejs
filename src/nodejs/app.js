460911ccf4110c6c62f392811ca9f2cded0aa198
Hoang Long <truonghoanglong305@gmail.com>
2021-06-10 13:10:55 +0700

    edit avatar

--- src/nodejs/app.js
+++ src/nodejs/app.js
@@ -10,14 +10,13 @@ const cookieParser = require('cookie-parser')
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json());
-app.use(cors());
+
 app.use(express.static(path.join(__dirname, 'assets')))
-app.options('*', cors());
-/* app.use(function (req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-}); */
+});
 const routers = require('./routes');
 routers(app)
 
