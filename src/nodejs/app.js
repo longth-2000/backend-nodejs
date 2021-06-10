cb1ddaf6ca5ce61f405be80d75064b67d6cdf90a
Hoang Long <truonghoanglong305@gmail.com>
2021-06-10 13:28:37 +0700

    edit avatar

--- src/nodejs/app.js
+++ src/nodejs/app.js
@@ -12,12 +12,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'assets')))
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-}) 
+app.use(cors({ origin: true }));
+
 const routers = require('./routes');
 routers(app)
 
