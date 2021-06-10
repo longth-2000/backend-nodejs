85bd3be8e9281a38e6acbddf5c1993ba14b94860
Hoang Long <truonghoanglong305@gmail.com>
2021-06-10 14:39:14 +0700

    edit avatar

--- src/nodejs/app.js
+++ src/nodejs/app.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'assets')))
+app.use(cors({ origin: true }));
 
 const routers = require('./routes');
 routers(app)
