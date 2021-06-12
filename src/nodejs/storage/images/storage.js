eb3ba7a12b73f6fd92218a5752a03acd7c49aa29
Hoang Long <truonghoanglong305@gmail.com>
2021-06-12 16:36:44 +0700

    fix cors error

--- src/nodejs/storage/images/storage.js
+++ src/nodejs/storage/images/storage.js
@@ -1,7 +1,7 @@
 var multer = require('multer')
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, `src/nodejs/assets/images/upload/${req.body.cookie}`)
+    cb(null, `src/nodejs/assets/images/upload`)
 
   },
   filename: function (req, file, cb) {
