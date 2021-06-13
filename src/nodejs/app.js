3361a5a0a7d228e20cfa3152307090d08e69d956
Hoang Long <truonghoanglong305@gmail.com>
2021-06-13 07:34:14 +0700

    fix cors

--- src/nodejs/app.js
+++ src/nodejs/app.js
@@ -12,11 +12,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'assets')))
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 const routers = require('./routes');
 routers(app)
